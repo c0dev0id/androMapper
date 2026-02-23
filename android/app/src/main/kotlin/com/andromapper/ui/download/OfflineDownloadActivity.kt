0d2822c5533eb0311820ad8f7c1101195c353df2
Stefan Hagen <sh+github@codevoid.de>
2026-02-23 14:37:11 +0100

    Fix build

--- android/app/src/main/kotlin/com/andromapper/ui/download/OfflineDownloadActivity.kt
+++ android/app/src/main/kotlin/com/andromapper/ui/download/OfflineDownloadActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.andromapper.R
 import com.andromapper.databinding.ActivityOfflineDownloadBinding
 
 class OfflineDownloadActivity : AppCompatActivity() {
